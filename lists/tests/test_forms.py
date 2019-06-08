b8ade45ed92926be23f5a1abbbfb9f1e216e61a0
Victor Soriano Mendoza <vsoriano@hotp.com>
2019-06-07 18:01:01 -0700

    start ch. 14 with simple forms

---
+++ lists/tests/test_forms.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from lists.forms import ItemForm
+
+class ItemFormTest(TestCase):
+
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
