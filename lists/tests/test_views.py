9f5f9ee148abdcc2334042830e975224db57bb57
Victor Soriano Mendoza <victor.sorianomendoza@gmail.com>
2019-06-21 10:09:28 -0700

    add duplicate item validation errors

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,9 +5,10 @@ from django.http import HttpRequest
 
 from lists.views import home_page
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
-
-from unittest import skip
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, ItemForm,
+    ExistingListItemForm, EMPTY_ITEM_ERROR
+)
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -91,7 +92,7 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
@@ -100,10 +101,9 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -112,7 +112,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
